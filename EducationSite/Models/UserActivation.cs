02291c0bda2cbaadd8a81324de4b2b339572b395
Rodrick <rodrickwamala@gmail.com>
2017-08-09 12:17:10 +0300

    revised authentication and starting to work on database relationships

---
+++ EducationSite/Models/UserActivation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EducationSite.Models
+{
+    public partial class UserActivation
+    {
+        public int CodeId { get; set; }
+        public string ActivationCode { get; set; }
+        public int Id { get; set; }
+        public virtual User User { get; set; }
+    }
+}
