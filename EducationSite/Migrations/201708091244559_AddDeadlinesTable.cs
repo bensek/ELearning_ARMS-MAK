1f0f5108708bd1dc07d355c0c2650ae25be97c69
bensek <bensek73@gmail.com>
2017-08-09 16:23:07 +0300

    Initial Deadlines

---
+++ EducationSite/Migrations/201708091244559_AddDeadlinesTable.cs
@@ -0,0 +1,16 @@
+namespace EducationSite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDeadlinesTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
